70b62fcaaa91a4bda5b22f6255a265424e46e692
astout55555 <alex.the.stout5@gmail.com>
2024-11-20 16:21:05 -0700

    Finished setting up and deploying the fullstack version of the phonebook app! Done with part 3b.

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 Part 3 exercises only (separated by necessity). Part 3 follow-along work is included in the main "fullstackopen" repo.
+
+It's a phonebook app!
---
+++ backend_phonebook/index.js
@@ -0,0 +1,134 @@
+const express = require('express');
+const app = express();
+
+app.use(express.static('dist'));
+
+const cors = require('cors');
+
+app.use(cors());
+
+app.use(express.json());
+
+const requestLogger = (request, response, next) => {
+  console.log('Method:', request.method);
+  console.log('Path:  ', request.path);
+  console.log('Body:  ', request.body);
+  console.log('---');
+  next();
+}
+
+app.use(requestLogger);
+
+const morgan = require('morgan');
+
+morgan.token('body', (request) => {
+  return JSON.stringify(request.body);
+});
+
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
+
+let persons = [
+  { 
+    "id": "1",
+    "name": "Arto Hellas", 
+    "number": "040-123456"
+  },
+  { 
+    "id": "2",
+    "name": "Ada Lovelace", 
+    "number": "39-44-5323523"
+  },
+  { 
+    "id": "3",
+    "name": "Dan Abramov", 
+    "number": "12-43-234345"
+  },
+  { 
+    "id": "4",
+    "name": "Mary Poppendieck", 
+    "number": "39-23-6423122"
+  },
+];
+
+app.get('/', (request, response) => {
+  response.send('<h1>Hello World!</h1>');
+});
+
+app.get('/info', (request, response) => {
+  const totalPeople = persons.length;
+  const currentDate = new Date();
+  response.send(
+    `<div>
+      <p>Phonebook has info for ${totalPeople} people</p>
+      <p>${currentDate}</p>
+    </div>`
+  );
+});
+
+app.get('/api/persons', (request, response) => {
+  response.json(persons);
+});
+
+app.get('/api/persons/:id', (request, response) => {
+  const id = request.params.id;
+  const person = persons.find(person => person.id === id);
+
+  if (person) {
+    response.json(person);
+  } else {
+    response.status(404).end();
+  }
+});
+
+app.delete('/api/persons/:id', (request, response) => {
+  const id = request.params.id;
+  persons = persons.filter(person => person.id !== id);
+
+  response.status(204).end();
+});
+
+function generateID() {
+  const newID = Math.floor(Math.random() * 999999999).toString();
+  console.log(newID);
+  return newID;
+}
+
+function isUnique(name) {
+  let names = persons.map(person => person.name);
+  return !names.includes(name);
+}
+
+app.post('/api/persons', (request, response) => {
+  const body = request.body;
+
+  if (!body.name || !body.number) {
+    return response.status(400).json({
+      error: 'name and/or number is missing'
+    });
+  } else if (!isUnique(body.name)) {
+    return response.status(400).json({
+      error: 'name must be unique'
+    });
+  }
+
+  const person = {
+    id: generateID(),
+    name: body.name,
+    number: body.number,
+  };
+
+  persons = persons.concat(person);
+
+  response.json(person);
+});
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' });
+}
+
+app.use(unknownEndpoint);
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
